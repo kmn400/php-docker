40acfb033a48108ccac65d32331ae151ad914e2b
minho <kmn400@nate.com>
2022-02-18 16:18:33 +0900

    db연결

---
+++ html/database/conn.php
@@ -0,0 +1,17 @@
+<?php
+$servername = "host.docker.internal";
+$username = "kmn400";
+$password = "1234";
+$mysql_database = 'kmh';
+$mysql_port = '52000';
+$mysql_charset = 'UTF8';
+
+// Create connection
+$conn = new mysqli($servername, $username, $password, $mysql_database, $mysql_port, $mysql_charset);
+
+// Check connection
+if ($conn->connect_error) {
+  die("Connection failed: " . $conn->connect_error);
+}
+echo "Connected successfully";
+?>
