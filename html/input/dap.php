1dd092af67f96d085628a27bfb1248322294910a
minho <kmn400@nate.com>
2022-02-18 14:44:21 +0900

    dap출력 코드

--- html/input/dap.php
+++ html/input/dap.php
@@ -1,8 +1,22 @@
 <?php
-    echo "first = ";
-    echo  $_GET['first'];
-    echo " / type = ";
-    echo  $_GET['type'];
-    echo " / last = ";
-    echo  $_GET['last'];
+    $last = $_GET['last'];
+    $first = $_GET['first'];
+    $type = $_GET['type'];
+
+    if($type == '1')//더하기
+    {
+        echo $first.'더하기'.$last.' 의 값은?';
+        echo $first+$last;
+    }
+    else if($type == '2')//빼기
+    {
+        echo $first.'빼기'.$last.' 의 값은?';
+        echo $first-$last;
+    }
+    else if($type == '3')//곱하기
+    {
+        echo $first.'곱하기'.$last.' 의 값은?';
+        echo $first*$last;
+    }
+
 ?>
