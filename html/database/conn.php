bf69373c5a6f07f80453b55f2677984fc888d270
minho <kmn400@nate.com>
2022-02-18 17:12:22 +0900

    connection

--- html/database/conn.php
+++ html/database/conn.php
@@ -7,4 +7,7 @@ $mysql_database = 'kmh';
 $mysql_port = '52000';
 $mysql_charset = 'UTF8';
 
+// Create connection
+$conn = new mysqli($servername, $username, $password,$mysql_database, $mysql_port, $mysql_charset);
+
 ?>
