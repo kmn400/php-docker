be6f4087ae82a3ec3102c1c10b4ad72aee66c86b
minho <kmn400@nate.com>
2022-02-18 11:54:28 +0900

    도커와 DB 연결 확인

---
+++ html/db.php
@@ -0,0 +1,33 @@
+<?php 
+
+$mysql_hostname = 'host.docker.internal';
+
+$mysql_username = 'kmn400';
+
+$mysql_password = '1234';
+
+$mysql_database = 'kmh';
+
+$mysql_port = '52000';
+
+$mysql_charset = 'UTF8';
+
+ 
+
+$connect = new mysqli($mysql_hostname, $mysql_username, $mysql_password, $mysql_database, $mysql_port, $mysql_charset);
+
+ 
+
+if($connect->connect_errno){
+
+    echo '[연결실패..] : '.$connect->connect_error.'';
+
+}else{
+
+    echo '[연결성공!]'.'<br>';
+
+}
+
+ 
+
+?>
